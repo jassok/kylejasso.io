ae6430d63535868c4682e241a0b628b4f8b0707a
Kyle Jasso <jassok@brolik.com>
2013-09-05 08:33:07 -0400

    hover menu

--- footer.php
+++ footer.php
@@ -1,2 +1,6 @@
+
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.hoverintent.js"></script>
+<script type="text/javascript" src="js/jquery.iconmenu.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,7 +1,60 @@
 <?php $page = "index"; ?>
 <?php include 'header.php'; ?>
+<script type="text/javascript">
+    $(function () {
+        var MAX_MODULE = 0;
+        $('nav').find('li').each(function () {
+            if($(this).height() > MAX_MODULE) {
+                MAX_MODULE = $(this).height();
+            }
+        });
+
+        $('nav').find('li').css('height',MAX_MODULE+"px");
+
+        $('#sti-menu').iconmenu();
+    });
+</script>
+
 <div class="centered">
-Body content
+    <nav>
+        <ul id="sti-menu" class="sti-menu">
+            <li data-hovercolor="#37c5e9">
+                <a href="#">
+                    <h2 data-type="mText" class="sti-item">About</h2>
+                    <p data-type="sText" class="sti-item">Skills, Education, and Experience</p>
+                    <span data-type="icon" class="sti-icon sti-icon-care sti-item"></span>
+                </a>
+            </li>
+            <li data-hovercolor="#ff395e">
+                <a href="#">
+                    <h2 data-type="mText" class="sti-item">Projects</h2>
+                    <p data-type="sText" class="sti-item">Client, and personal Work</p>
+                    <span data-type="icon" class="sti-icon sti-icon-alternative sti-item"></span>
+                </a>
+            </li>
+            <li data-hovercolor="#57e676">
+                <a href="#">
+                    <h2 data-type="mText" class="sti-item">Labs</h2>
+                    <p data-type="sText" class="sti-item">Just show me the code</p>
+                    <span data-type="icon" class="sti-icon sti-icon-info sti-item"></span>
+                </a>
+            </li>
+            <li data-hovercolor="#d869b2">
+                <a href="#">
+                    <h2 data-type="mText" class="sti-item">Blog</h2>
+                    <p data-type="sText" class="sti-item">Ramblings of a coder</p>
+                    <span data-type="icon" class="sti-icon sti-icon-family sti-item"></span>
+                </a>
+            </li>
+            <li data-hovercolor="#ffdd3f">
+                <a href="#">
+                    <h2 data-type="mText" class="sti-item">Contact</h2>
+                    <p data-type="sText" class="sti-item">Get in-touch with me</p>
+                    <span data-type="icon" class="sti-icon sti-icon-technology sti-item"></span>
+                </a>
+            </li>
+        </ul>
+    </nav>
 </div>
 
 <?php include 'footer.php'; ?>
